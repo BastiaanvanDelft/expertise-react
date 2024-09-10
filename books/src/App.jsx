51e4832ed150d7567ca48796a8ce0e12d0d8d8a7
BastiaanvanDelft <9019711@student.zadkine.nl>
2024-09-10 11:32:55 +0200

    initial commit

---
+++ books/src/App.jsx
@@ -0,0 +1,14 @@
+import Book from "./components/Book";
+import BookList from "./components/BookList";
+function App() {
+  
+
+  return (
+    <>
+    <h1>Amazon Bestsellers</h1>
+      <BookList/>
+    </>
+  );
+}
+
+export default App
---
+++ books/src/components/BookList.jsx
@@ -0,0 +1,11 @@
+import Book from "./Book";
+const BookList = () => {
+    return (
+    <>
+        <Book image="afbeelding 1" title="hallo" author="Man" />
+        <Book image="afbeelding 2" title="Grappige Goocheltrucs" author="Hans Klok" />
+    </>
+    );
+}
+ 
+export default BookList;
