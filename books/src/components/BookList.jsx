8146704ffd31fd814402747acf770d7341d817b0
BastiaanvanDelft <9019711@student.zadkine.nl>
2024-09-24 10:35:11 +0200

    usestate

--- books/src/components/BookList.jsx
+++ books/src/components/BookList.jsx
@@ -1,11 +1,20 @@
+import { useState } from "react";
 import Book from "./Book";
 const BookList = () => {
+
+    const [books, setBooks] = useState([
+        {title: "Hallo", image: "afbeelding 1", author: "Man"},
+        {title: "Grappige Trucs", image: "afbeelding 2", author: "Hans Klok"},
+        {title: "Henk de Steen", image: "afbeelding 3", author: "Jan-Peter"}
+    ])
+
     return (
-    <>
-        <Book image="afbeelding 1" title="hallo" author="Man" />
-        <Book image="afbeelding 2" title="Grappige Goocheltrucs" author="Hans Klok" />
-        <Book image="afbeelding 3" title="Henk de Steen" author="Jan-Peter" />
-    </>
+        <section>
+            {books.map((book) => (
+                <Book title={book.title} author={book.author} image={book.image}/>
+            ))}
+
+        </section>
     );
 }
  
